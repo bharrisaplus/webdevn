78396fd85d331787e272c133a00e5960e8486a97
Barry <bharrisaplus@gmail.com>
2026-01-24 23:42:46 -0500

    Add methods for printing cli config and issues. Clean up cli

--- source/cli.nim
+++ source/cli.nim
@@ -1,16 +1,39 @@
-import std/[paths, os, parseopt, dirs, sequtils, strutils, strformat, files, net, nativesockets, sugar]
+import std/[paths, os, parseopt, dirs, sequtils, strutils, strformat, files, net, nativesockets]
 
-type cliOptions = object
+type cliConfig = object
   basePath: Path
   listenPort: int
   indexFile: Path
   inSilence: bool
   writeLog: bool
 
+proc print_cliConfig(thingy: cliConfig) =
+  var outputStr = "Cli Config:\n"
 
-proc getCliOptions(osCliParams: seq[string]): (cliOptions, seq[string]) =
+  outputStr.add(&"  - basePath => '{thingy.basePath}'\n")
+  outputStr.add(&"  - listenPort => {thingy.listenPort}\n")
+  outputStr.add(&"  - indexFile => '{thingy.indexFile}'\n")
+  outputStr.add(&"  - inSilence => {thingy.inSilence}\n")
+  outputStr.add(&"  - writeLog => {thingy.writeLog}\n")
+
+  echo outputStr
+
+
+proc print_cliConfigIssues(stuff: seq[string]) =
+  var outputStr = &"Cli Issue(s) - [{stuff.len}]:\n"
+
+  if stuff.len == 0:
+    outputStr.add("  * No issues!\n")
+  else:
+    for issue in stuff:
+      outputStr.add(&"  * {issue}\n")
+
+  echo outputStr
+
+
+proc buildCliConfig*(osCliParams: seq[string]): (cliConfig, seq[string]) =
   var parseProblems: seq[string]
-  var maybeOptions = cliOptions(
+  var maybeConfig = cliConfig(
     basePath: paths.getCurrentDir(),
     listenPort: 0,
     indexFile: Path("index.html"),
@@ -21,9 +44,9 @@ proc getCliOptions(osCliParams: seq[string]): (cliOptions, seq[string]) =
   var maybeIndexFile: Path
   var checkSocket: Socket
 
-  for optkind, optarg, optinput in parseopt.getopt(cmdline = osCliParams, shortNoVal = {'v', 'l'}, longNoVal = @["verbose", "log"]):
+  for optkind, optarg, optinput in getopt(cmdline = osCliParams, shortNoVal = {'v', 'l'}, longNoVal = @["verbose", "log"]):
     if optkind == cmdArgument or optkind == cmdEnd:
-        discard
+        continue
     else: # cmdShortOption and cmdLongOption:
       case optarg: # A valid path is required
         of "d", "dir":
@@ -33,55 +56,60 @@ proc getCliOptions(osCliParams: seq[string]): (cliOptions, seq[string]) =
 
           maybeBasePath = paths.expandTilde(maybeBasePath)
 
-          if dirs.dirExists(maybeBasePath):
-            maybeOptions.basePath = maybeBasePath
+          if maybeBasePath.dirExists():
+            maybeConfig.basePath = maybeBasePath
 
         of "p", "port": # Convert input to an int and verify within general port range
-          if not strutils.isEmptyOrWhitespace(optinput) and optinput.allIt(strutils.isDigit(it)):
+          if not optinput.isEmptyOrWhitespace() and optinput.allIt(it.isDigit()):
             try:
-              var maybeListenPort = strutils.parseInt(optinput)
-              if maybeListenPort > 0 and maybeListenPort < 65535:
-                maybeOptions.listenPort = maybeListenPort
-            except ValueError as vE:
-              parseProblems.add(strformat.fmt"Issue with '-p/--port' ~> {vE.name}: {vE.msg}")
+              var maybeListenPort = optinput.parseInt()
+
+              if maybeListenPort > 0 and maybeListenPort <= 65535:
+                maybeConfig.listenPort = maybeListenPort
+              else:
+                parseProblems.add("Issue with '-p/--port' ~> ValueError: Should be 0 .. 65535")
+            except ValueError as valE:
+              parseProblems.add(&"Issue with '-p/--port' ~> {valE.name}: {valE.msg}")
           else:
-            parseProblems.add(
-              "Issue with '-p/--port' ~> Value should be integer representing port (0 - 65535)"
-            )
+            parseProblems.add("Issue with '-p/--port' ~> ValueError: Should be integer")
 
         of "i", "index":
           maybeIndexFile = Path(optinput)
           paths.normalizePath(maybeIndexFile)
 
         of "v", "verbose":
-          maybeOptions.inSilence = false
+          maybeConfig.inSilence = false
 
         of "l", "log":
-          maybeOptions.writeLog = true
+          maybeConfig.writeLog = true
 
   # Check for index file in the basePath
   #
-  if files.fileExists(maybeOptions.basePath / maybeIndexFile):
-    maybeOptions.indexFile = maybeIndexFile
+  if fileExists(maybeConfig.basePath / maybeIndexFile):
+    maybeConfig.indexFile = maybeIndexFile
   else:
-    if not files.fileExists(maybeOptions.basePath / maybeOptions.indexFile):
-      parseProblems.add(
-        "Issue with '-i/--index' ~> IOError: Index file, index not found within directory, -d/--dir"
+    if not fileExists(maybeConfig.basePath / maybeConfig.indexFile):
+      parseProblems.add(&"Issue with '-i/--index' ~> IOError: Index file [{maybeIndexFile},{maybeConfig.indexFile}] index not found within directory"
       )
 
   # Check that port is available
   #
   try:
-    checkSocket = net.newSocket()
-    checkSocket.bindAddr(maybeOptions.listenPort.Port)
+    checkSocket = newSocket()
+    checkSocket.bindAddr(maybeConfig.listenPort.Port)
   except OSError as osE:
     parseProblems.add(&"Issue with '-p/--port' ~> {osE.name}: {osE.msg}")
   finally:
     checkSocket.close()
 
-  return (maybeOptions, parseProblems)
+  # Output results
+  #
+  if not maybeConfig.inSilence:
+    print_cliConfig(maybeConfig)
+    print_cliConfigIssues(parseProblems)
 
-let cliOptionsResult = getCliOptions(os.commandLineParams())
+  return (maybeConfig, parseProblems)
 
-dump(cliOptionsResult[0])
-dump(cliOptionsResult[1])
+
+when isMainModule:
+  discard buildCliConfig(commandLineParams())
