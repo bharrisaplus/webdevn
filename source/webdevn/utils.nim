610519e7c2f44777010e34c600ab1825bd5daac3
Barry <bharrisaplus@gmail.com>
2026-01-25 02:13:00 -0500

    Reorganize print methods to separate misc file

---
+++ source/webdevn/utils.nim
@@ -0,0 +1,26 @@
+import std/[strformat]
+
+import type_defs
+
+proc print_config* (title: string = "webdevn", thingy: webdevnConfig) =
+  var outputStr = &"{title} Config:\n"
+
+  outputStr.add(&"  - basePath => '{thingy.basePath}'\n")
+  outputStr.add(&"  - listenPort => {thingy.listenPort}\n")
+  outputStr.add(&"  - indexFile => '{thingy.indexFile}'\n")
+  outputStr.add(&"  - inSilence => {thingy.inSilence}\n")
+  outputStr.add(&"  - writeLog => {thingy.writeLog}\n")
+
+  echo outputStr
+
+
+proc print_issues* (title: string = "webdevn", stuff: seq[string]) =
+  var outputStr = &"{title} Issue(s) - [{stuff.len}]:\n"
+
+  if stuff.len == 0:
+    outputStr.add("  * No issues!\n")
+  else:
+    for issue in stuff:
+      outputStr.add(&"  * {issue}\n")
+
+  echo outputStr
