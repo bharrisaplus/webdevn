9ae4ba648cf9cd6dc5b95f5930a4889cc59180cf
Barry <bharrisaplus@gmail.com>
2026-01-26 01:10:59 -0500

    Example for dynamically generate http headers

--- source/webdevn/localserver.nim
+++ source/webdevn/localserver.nim
@@ -1,4 +1,26 @@
-#import std/[paths, dirs, files, strutils, asynchttpserver, asyncdispatch, asyncfile]
+import std/[sugar, times, httpcore, mimetypes]
+import checksums/md5
+
+let webdevnBaseHeaders :seq[tuple[key: string, val: string]] = @{
+  "Server": "webdevn; nim/c",
+  "Cache-Control": "no-cache",
+  "Clear-Site-Data": "*"
+}
 
 when isMainModule:
   echo "No localserver-servin happening here yet"
+
+  let current_time = now().utc()
+  let mimeLookup = newMimeTypes()
+
+  let localserverContent = "Hello, World"
+
+  var generatedHeaders :seq[tuple[key: string, val: string]] = @{
+    "Content-Type": mimeLookup.getMimeType("html") & "; charset=utf-8",
+    "Date": current_time.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT",
+    "ETag": getMD5(localserverContent),
+  }
+
+  var defg = newHttpHeaders(generatedHeaders & webdevnBaseHeaders)
+
+  dump(defg)
