13d058cb159611a105ddd265cf2fd7ed78862620
Barry <bharrisaplus@gmail.com>
2026-01-26 23:38:45 -0500

    Simplify local server example

--- source/webdevn/localserver.nim
+++ source/webdevn/localserver.nim
@@ -1,26 +1,20 @@
 import std/[sugar, times, httpcore, mimetypes]
 import checksums/md5
 
-let webdevnBaseHeaders :seq[tuple[key: string, val: string]] = @{
-  "Server": "webdevn; nim/c",
-  "Cache-Control": "no-cache",
-  "Clear-Site-Data": "*"
-}
 
 when isMainModule:
-  echo "No localserver-servin happening here yet"
+  let m = newMimeTypes()
 
-  let current_time = now().utc()
-  let mimeLookup = newMimeTypes()
-
-  let localserverContent = "Hello, World"
-
-  var generatedHeaders :seq[tuple[key: string, val: string]] = @{
-    "Content-Type": mimeLookup.getMimeType("html") & "; charset=utf-8",
-    "Date": current_time.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT",
-    "ETag": getMD5(localserverContent),
+  var otfHeaders :seq[tuple[key: string, val: string]] = @{
+    "Content-Type": m.getMimeType("html") & "; charset=utf-8",
+    "Date": now().utc().format("ddd, dd MMM yyyy HH:mm:ss") & " GMT",
+    "ETag": getMD5("Hello, World"),
   }
 
-  var defg = newHttpHeaders(generatedHeaders & webdevnBaseHeaders)
+  var defg = newHttpHeaders(otfHeaders & @{
+    "Server": "webdevn; nim/c",
+    "Cache-Control": "no-cache",
+    "Clear-Site-Data": "*"
+  })
 
   dump(defg)
