51856c2c28767cc023048d795b7a9006b553f6a9
Barry <bharrisaplus@gmail.com>
2026-01-26 23:33:05 -0500

    Clean up main module with styled types and renamed variables

--- source/webdevn.nim
+++ source/webdevn.nim
@@ -2,12 +2,12 @@ import std/[os]
 import webdevn/[type_defs, cli, utils]
 
 when isMainModule:
-  var webdevnIssues: seq[string]
-  let (webdevnCliConfig, cliIssues) = configFromCLi(commandLineParams())
+  var webdevnIssues :seq[string]
+  let (cliConfig, cliIssues) = configFromCLi(commandLineParams())
 
   if cliIssues.len > 0:
     webdevnIssues.add(cliIssues)
 
-  if not webdevnCliConfig.inSilence:
-    print_config("Cli", webdevnCliConfig)
-    print_Issues("Cli", webdevnIssues)
+  if not cliConfig.inSilence:
+    print_config("Cli", cliConfig)
+    print_Issues("Cli", cliIssues)
