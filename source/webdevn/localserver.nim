4d72979d32f41713edc342bb07af0bd0ef412bf1
Barry <bharrisaplus@gmail.com>
2026-01-27 02:37:58 -0500

    Make controlC exit method that works while loop is running

--- source/webdevn/localserver.nim
+++ source/webdevn/localserver.nim
@@ -1,7 +1,7 @@
-from std/httpcore import newHttpHeaders, `$`
+from std/httpcore import HttpHeaders, newHttpHeaders, `$`
 from std/mimetypes import MimeDB, newMimeTypes, getMimeType
 from std/asynchttpserver import AsyncHttpServer, newAsyncHttpServer
-from system import debugEcho
+from system import setControlCHook, quit
 
 from checksums/md5 import getMD5
 
@@ -22,8 +22,11 @@ proc newWebdevnLocalServer* (someMilieu :webdevnMilieu) :localServer =
 
 
 when isMainModule:
-  let loser = newWebdevnLocalServer(webdevnMilieu(runConf: defaultWebdevnConfig()))
+  let loser = newWebdevnLocalServer(webdevnMilieu(runConf: devWebdevnConfig()))
   let grettingContent = "Hello, World"
+
+  if not loser.serverMilieu.runConf.inSilence:
+    print_milieu("localserver", loser.serverMilieu)
 
   var otfHeaders = newHttpHeaders(loser.serverMilieu.baseHeaders & mect_stamp(
     loser.mimeLookup.getMimeType(loser.serverMilieu.runConf.indexFileExt),
@@ -31,5 +34,15 @@ when isMainModule:
     grettingContent.len
   ))
 
-  print_milieu("localserver", loser.serverMilieu)
-  debugEcho(otfHeaders)
+  echo "Stamped Headers: " & $otfHeaders
+
+  setControlCHook(proc() {.noconv.} =
+    if not loser.serverMilieu.runConf.inSilence:
+      print_milieu("\nlocalserver", loser.serverMilieu)
+
+    quit("\nwebdevn - shutting down...\n", 0)
+  )
+
+  echo "\nPress 'Ctrl+C' to exit"
+  while true:
+    discard
