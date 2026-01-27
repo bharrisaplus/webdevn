60dafe31755828d5550c0de8caedb47c5615f370
Barry <bharrisaplus@gmail.com>
2026-01-26 23:03:49 -0500

    Change style in type_defs to use colon next to type instead of identifier

--- source/webdevn/type_defs.nim
+++ source/webdevn/type_defs.nim
@@ -2,8 +2,8 @@ from std/paths import Path
 from std/net import Port
 
 type webdevnConfig* = object
-  basePath*: Path
-  listenPort*: Port
-  indexFile*: string
-  inSilence*: bool
-  writeLog*: bool
+  basePath* :Path
+  listenPort* :Port
+  indexFile* :string
+  inSilence* :bool
+  writeLog* :bool
