5c13c432b91820608b1ad3b40a2d38b2b58b0f63
Barry <bharrisaplus@gmail.com>
2026-02-02 20:10:47 -0500

    Add test for localserver module

--- spec/localserver_spec.nim
+++ spec/localserver_spec.nim
@@ -1,9 +1,11 @@
 import std/unittest
 from std/paths import Path, absolutePath
-from std/net import Port
-from std/httpcore import HttpHeaders, `$`
-from std/strformat import `&`
+from std/uri import Uri, parseUri
+from std/asynchttpserver import Request
+from std/httpcore import HttpHeaders, Http200, Http404, `==`
+from asyncdispatch import waitFor
 from std/tables import `[]`
+from std/strutils import startsWith, unindent, splitWhitespace, join
 
 import ../source/webdevn/[type_defs, localserver]
 
@@ -23,6 +25,11 @@ proc loserSpecWebDevnConfig (lBasePath :string, lIndexfile :string = "index.html
     inSilence: true,
     writeLog: false,
     zeroHost: false
+  )
+
+proc loserRequest (loserUri :Uri) :Request =
+  return Request(
+    url: loserUri
   )
 
 
@@ -50,3 +57,48 @@ suite "LocalServer_BS":
     check:
       maybeSolution_1.table["content-type"] == @["text/html; charset=utf-8"]
       maybeSolution_2.table["content-type"] == @["image/jpeg"]
+
+
+  test "Should have good response code if request is looked up and read successfully":
+    let
+      swearUrl_1 = parseUri("http://localhost:54321/")
+      swearUrl_2 = parseUri("http://localhost:54321/main.css")
+      swearMilieu = defaultWebdevnMilieu(
+        loserSpecWebDevnConfig(lBasePath = "./spec/appa/has_index")
+      )
+
+    let
+      maybeSolution_1 = waitFor localserver.aio_for(loserRequest(swearUrl_1), swearMilieu, quietLoserSpecScribe)
+      maybeSolution_2 = waitFor localserver.aio_for(loserRequest(swearUrl_2), swearMilieu, quietLoserSpecScribe)
+
+    check:
+      maybeSolution_1.responseCode == Http200
+      maybeSolution_1.responseHeaders.table["content-type"] == @["text/html; charset=utf-8"]
+      maybeSolution_1.responseContent.splitWhitespace().join("") == """<!DOCTYPEhtml><htmllang="en"><head><title>webdevndemo</title><metacharset="UTF-8"><metaname="viewport"content="initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"></head><body><h2>HelloWorld</h2></body></html>"""
+
+      maybeSolution_2.responseCode == Http200
+      maybeSolution_2.responseHeaders.table["content-type"] == @["text/css; charset=utf-8"]
+      maybeSolution_2.responseContent.splitWhitespace().join("") == "html{width:100%;height:100%;}body{height:calc(100%-16px);width:calc(100%-16px);margin:8px;align-content:center;justify-content:center;}h2{font-size:24px;font-weight:bold;}"
+
+
+  test "Should have bad response code if request is not looked up and read successfully":
+    let
+      swearUrl_1 = parseUri("http://localhost:54321/")
+      swearMilieu_1 = defaultWebdevnMilieu(
+        loserSpecWebDevnConfig(lBasePath = "./spec/appa/has_no_index")
+      )
+      swearUrl_2 = parseUri("http://localhost:54321/index.html")
+      swearMilieu_2 = defaultWebdevnMilieu(
+        loserSpecWebDevnConfig(lBasePath = "./spec/appa/has_index_custom")
+      )
+
+    let
+      maybeSolution_1 = waitFor localserver.aio_for(loserRequest(swearUrl_1), swearMilieu_1, quietLoserSpecScribe)
+      maybeSolution_2 = waitFor localserver.aio_for(loserRequest(swearUrl_2), swearMilieu_2, quietLoserSpecScribe)
+
+    check:
+      maybeSolution_1.responseCode == Http404
+      maybeSolution_1.responseHeaders.table["content-type"] == @["text/html; charset=utf-8"]
+
+      maybeSolution_2.responseCode == Http404
+      maybeSolution_2.responseHeaders.table["content-type"] == @["text/html; charset=utf-8"]
