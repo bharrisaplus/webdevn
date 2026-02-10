deb4e91aab3ced3a265f68a37b17a4c213883512
Barry <bharrisaplus@gmail.com>
2026-02-10 16:50:33 -0500

    Clean up

--- source/webdevn/meta.nim
+++ source/webdevn/meta.nim
@@ -6,10 +6,11 @@ const
   appDepVersions* {.strdefine.} :string = "unknown"
   # Since read at compile time the path is relative to this file not where run from
   webdevnFavicon* :string = staticRead("assets/img/raster/icon.ico")
-  appVersionBlurb* = if (appDepVersions == "unknown" or appDepVersions.isEmptyOrWhitespace()):
-    "version: " & appVersion
-  else:
-    "version: " & appVersion & "\npackage dependencies: " & appDepVersions
+  appVersionBlurb* = 
+    if (appDepVersions == "unknown" or appDepVersions.isEmptyOrWhitespace()):
+      "version: " & appVersion
+    else:
+      "version: " & appVersion & "\npackage dependencies: " & appDepVersions
 
   helpManual* = &"""
 source lang: nim
